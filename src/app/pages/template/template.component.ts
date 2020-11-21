dae42744e89e86c0b21f1bfb54807987c28b1a30
mabvmex <mabvmex@gmail.com>
2020-11-20 23:08:31 -0600

    Servicio pais - select y validaciones - radio buttons

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,37 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
+
 export class TemplateComponent implements OnInit {
 
   usuario = {
     nombre: 'Miguel ',
     apellido: 'Barrera',
-    email: 'Dax@mex.com',
+    email: 'dax@mex.com',
+    pais : ''
   };
 
 
+  paises: any[] = [];
 
-  constructor() { }
+
+  // tslint:disable-next-line: no-shadowed-variable
+  constructor( private PaisService: PaisService ) { }
 
   ngOnInit(): void {
+  this.PaisService.getPaises()
+  .subscribe(paises => {
+    this.paises = paises;
+
+    this.paises.unshift({
+      nombre: '- Seleccione país -',
+      codigo: ''
+    });
+
+    // console.log('paises =>', paises);
+  });
+
   }
 
+  // tslint:disable-next-line: typedef
   guardar(forma: NgForm ) {
-    console.log(forma);
+    console.log('forma =>', forma);
 
-    if(forma.invalid) {
+    if (forma.invalid) {
       Object.values(forma.controls).forEach( control => {
         control.markAsTouched();
       });
       return;
     }
 
-    console.log(forma.value);
+    console.log('forma.value =>', forma.value);
   }
 
 
