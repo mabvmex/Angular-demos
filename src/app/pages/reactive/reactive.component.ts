4b95d7d49f90b5e1bdfc1f6d18e2c8a456402dd5
mabvmex <mabvmex@gmail.com>
2020-11-23 01:23:46 -0600

    Reactivo: reset y carga de la data inicial

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -11,63 +11,78 @@ export class ReactiveComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.crearFormulario();
+    this.cargarDataAlFormulario();
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
-  // tslint:disable-next-line: typedef
-  get nombreNoValido() { return this.forma.get('nombre').invalid && this.forma.get('nombre').touched; }
-  // tslint:disable-next-line: typedef
-  get nombreValido() { return this.forma.get('nombre').valid && this.forma.get('nombre').touched; }
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
 
+  get nombreValido() {
+    return this.forma.get('nombre').valid && this.forma.get('nombre').touched;
+  }
 
-  // tslint:disable-next-line: typedef
   get apellidoNoValido() {
-    return (this.forma.get('apellido').invalid && this.forma.get('apellido').touched);
+    return (
+      this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+    );
   }
-  // tslint:disable-next-line: typedef
+
   get apellidoValido() {
-    return (this.forma.get('apellido').valid && this.forma.get('apellido').touched);
+    return (
+      this.forma.get('apellido').valid && this.forma.get('apellido').touched
+    );
   }
 
-
-  // tslint:disable-next-line: typedef
   get correoNoValido() {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
-  // tslint:disable-next-line: typedef
+
   get correoValido() {
     return this.forma.get('correo').valid && this.forma.get('correo').touched;
   }
 
-
-  // tslint:disable-next-line: typedef
   get coloniaNoValido() {
     return (
-      this.forma.get('direccion.colonia').invalid && this.forma.get('direccion.colonia').touched
+      this.forma.get('direccion.colonia').invalid &&
+      this.forma.get('direccion.colonia').touched
     );
   }
-  // tslint:disable-next-line: typedef
+
   get coloniaValido() {
-    return this.forma.get('direccion.colonia').valid && this.forma.get('direccion.colonia').touched;
+    return (
+      this.forma.get('direccion.colonia').valid &&
+      this.forma.get('direccion.colonia').touched
+    );
   }
 
-
-  // tslint:disable-next-line: typedef
   get ciudadNoValido() {
-    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
-  }
-  // tslint:disable-next-line: typedef
-  get ciudadValido() {
-    return this.forma.get('direccion.ciudad').valid && this.forma.get('direccion.ciudad').touched;
+    return (
+      this.forma.get('direccion.ciudad').invalid &&
+      this.forma.get('direccion.ciudad').touched
+    );
   }
 
-  // tslint:disable-next-line: typedef
+  get ciudadValido() {
+    return (
+      this.forma.get('direccion.ciudad').valid &&
+      this.forma.get('direccion.ciudad').touched
+    );
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', [Validators.required, Validators.minLength(5)]],
-      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]],
+      correo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
       direccion: this.fb.group({
         colonia: ['', [Validators.required, Validators.minLength(2)]],
         ciudad: ['', [Validators.required, Validators.minLength(2)]],
@@ -75,18 +90,35 @@ export class ReactiveComponent implements OnInit {
     });
   }
 
-  // tslint:disable-next-line: typedef
+  cargarDataAlFormulario() {
+    // this.forma.setValue({
+    this.forma.reset({
+      nombre: 'Miguel',
+      apellido: 'Barrera',
+      correo: 'mabvmet@gmail.com',
+      direccion: {
+        colonia: 'Centro',
+        ciudad: 'Tula',
+      },
+    });
+  }
+
   guardar() {
-
     if (this.forma.invalid) {
-      return Object.values(this.forma.controls).forEach(control => {
-
+      return Object.values(this.forma.controls).forEach((control) => {
         if (control instanceof FormGroup) {
-          Object.values(control.controls).forEach(control => control.markAllAsTouched());
+          Object.values(control.controls).forEach((control) =>
+            control.markAllAsTouched()
+          );
         } else {
           control.markAsTouched();
         }
       });
     }
+
+    // posteo de la información
+    this.forma.reset({
+      nombre: 'Ahora nombre',
+    });
   }
 }
