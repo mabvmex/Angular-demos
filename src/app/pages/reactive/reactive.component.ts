334e5ee255485072d306a6e3cd3b80442b45593f
mabvmex <mabvmex@gmail.com>
2020-11-26 00:20:10 -0600

    Reactivo: validadoresPass  visual correcto

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -42,7 +42,7 @@ export class ReactiveComponent implements OnInit {
   get passwordValido()   { return this.forma.get('password').valid && this.forma.get('password').touched; } 
   get passwordNoValido() { return this.forma.get('password').invalid && this.forma.get('password').touched; } 
   
-  get repeatPasswordValido() { return this.forma.get('repeatPassword').valid}
+  get repeatPasswordValido() { return this.forma.get('repeatPassword').valid && this.forma.get('repeatPassword').touched;}
   get repeatPasswordNoValido() { 
     const password = this.forma.get('password').value;
     const repeatPassword = this.forma.get('repeatPassword').value;
@@ -53,14 +53,14 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre:   [ 'Miguel', [ Validators.required, Validators.minLength(2)] ],
-      apellido: [ 'herrerax', [ Validators.required, Validators.minLength(2), this.validadores.noHerrera ] ],
-      correo:   [ 'a@a.com', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$') ] ],
+      nombre:   [ '', [ Validators.required, Validators.minLength(2)] ],
+      apellido: [ '', [ Validators.required, Validators.minLength(2), this.validadores.noHerrera ] ],
+      correo:   [ '', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$') ] ],
       password:     [ '', [Validators.required, Validators.minLength(5) ] ],
       repeatPassword:   [ '', [Validators.required] ],
       direccion: this.fb.group({ 
-        colonia:  ['Centro', [ Validators.required, Validators.minLength(2)] ], 
-        ciudad:   ['CDMX', [ Validators.required, Validators.minLength(2)] ]
+        colonia:  ['', [ Validators.required, Validators.minLength(2)] ], 
+        ciudad:   ['', [ Validators.required, Validators.minLength(2)] ]
       }),
       pasatiempos: this.fb.array([]),
     },{
